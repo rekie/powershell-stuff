61505fada172767cf15b40be73fa124c5909ad22
Reinhard Kiesswetter <reinhard@scerus.com>
2019-12-19 16:54:39 +0100

    Add script enabling access to port 5589

---
+++ enable-winrm-access.ps1
@@ -0,0 +1,1 @@
+Get-NetFirewallPortFilter | ?{$_.LocalPort -eq 5985 } | Get-NetFirewallRule | ?{ $_.Direction -eq "Inbound" -and $_.Profile -eq "Public" -and $_.Action -eq "Allow"} | Set-NetFirewallRule -RemoteAddress "Any"
