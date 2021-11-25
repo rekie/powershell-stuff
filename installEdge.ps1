081de1e3c11e862a091228c12be32b89986ab2ba
Reinhard Kiesswetter <reinhard@scerus.com>
2021-11-25 18:13:33 +0100

    Add script installing Edge

---
+++ installEdge.ps1
@@ -0,0 +1,5 @@
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+md -Path $env:temp\edgeinstall -erroraction SilentlyContinue | Out-Null
+$Download = join-path $env:temp\edgeinstall MicrosoftEdgeEnterpriseX64.msi
+Invoke-WebRequest 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/a2662b5b-97d0-4312-8946-598355851b3b/MicrosoftEdgeEnterpriseX64.msi'  -OutFile $Download
+Start-Process "$Download" -ArgumentList "/quiet"
